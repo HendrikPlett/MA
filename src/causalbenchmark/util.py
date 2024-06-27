bce101f638ecf353f6c3f68a13db16694709cf83
HendrikPlett <hendrik.plett@web.de>
2024-06-27 02:24:09 +0200

    Added validity checker

--- src/causalbenchmark/util.py
+++ src/causalbenchmark/util.py
@@ -19,7 +19,52 @@ def measure_time(func):
 
 
 #------------------------------------------------------
-# Dataframe operations
+# List operations
+
+def has_duplicates(l: list) -> bool:
+    return len(l) != len(set(l))
+
+def enforce_no_duplicates(l_of_l: list[list]):
+    if any([has_duplicates(l) for l in l_of_l]):
+        raise ValueError("Duplicates in at least one passed list.")
+
+def give_superlist(first: list, second: list) -> list:
+    enforce_no_duplicates([first, second])
+    first_set = set(first)
+    second_set = set(second)
+    if first_set.issuperset(second_set):
+        return first
+    elif second_set.issuperset(first_set):
+        return second
+    else:
+        raise ValueError("No Variable list contains the other.")
+
+def give_sublist(first: list, second: list) -> list:
+    enforce_no_duplicates([first, second])
+    first_set = set(first)
+    second_set = set(second)
+    if first_set.issubset(second_set):
+        return first
+    elif second_set.issubset(first_set):
+        return second
+    else:
+        raise ValueError("No Variable list contains the other.")
+
+def variables_increase(first: list, second: list) -> bool:
+    return (first == give_sublist(first, second))
+    
+def same_order(first: list, second: list) -> bool:
+    try: 
+        superlist = give_superlist(first, second)
+        sublist = give_sublist(first, second)
+    except ValueError:
+        return False
+    superlist_reduced = [el for el in superlist if el in sublist]
+    return superlist_reduced == sublist
+
+
+#------------------------------------------------------
+# Data Dataframe operations
 
 def pool_dfs(dfs: Iterable[pd.DataFrame]) -> pd.DataFrame:
     """
@@ -54,7 +99,7 @@ def same_columns(dfs: Iterable[pd.DataFrame]) -> bool:
     ref_columns = list(dfs[0].columns)
     same_names_and_order = all(df.columns.tolist() == ref_columns for df in dfs)
     return same_names_and_order
-
+    
 
 def bootstrap_sample(datasets: Iterable[pd.DataFrame], sample_sizes: Iterable[Union[int, float]], seed):
 
@@ -75,30 +120,33 @@ def bootstrap_sample(datasets: Iterable[pd.DataFrame], sample_sizes: Iterable[Un
 
 
 #------------------------------------------------------
-# List operations
+# Adjacency matrix dataframe operations
 
-def give_superlist(first: list, second: list):
-    first_set = set(first)
-    second_set = set(second)
-    if first_set.issuperset(second_set):
-        return first
-    elif second_set.issuperset(first_set):
-        return second
-    else:
-        raise ValueError("No Variable list contains the other")
+def reduce_to_size(df: pd.DataFrame, reduce_to: pd.DataFrame):
+    enforce_valid_adj_mat(df)
+    enforce_valid_adj_mat(reduce_to)
+    common_var = df.columns.intersection(reduce_to)
+    df_reduced = df.loc[common_var, common_var]
+    return df_reduced
 
-def give_sublist(first: list, second: list):
-    first_set = set(first)
-    second_set = set(second)
-    if first_set.issubset(second_set):
-        return first
-    elif second_set.issubset(first_set):
-        return second
-    else:
-        raise ValueError("No Variable list contains the other")
+def enforce_valid_bstr_adj_mat(graph: pd.DataFrame):
+    enforce_valid_adj_mat(graph)
+    if not ((graph >= 0) & (graph <= 1)).all().all():
+        raise ValueError("Entries outside of [0,1].")
+    
+def enforce_valid_diff_adj_mat(graph: pd.DataFrame):
+    enforce_valid_adj_mat(graph)
+    if not ((graph >= -1) & (graph <= 1)).all().all():
+        raise ValueError("Entries outside of [-1,1].")
 
-def same_order(first: list, second: list):
-    superlist = give_superlist(first, second)
-    sublist = give_sublist(first, second)
-    superlist_reduced = [el for el in superlist if el in sublist]
-    return superlist_reduced == sublist
+def enforce_binary_adj_mat(graph: pd.DataFrame):
+    enforce_valid_adj_mat(graph)
+    if not graph.isin([0,1]).all().all():
+        raise ValueError("Not all entries binary.")
+
+def enforce_valid_adj_mat(graph: pd.DataFrame):
+    if not same_columns([graph, graph.transpose()]):
+        raise ValueError("Different index/column names.")
+    enforce_no_duplicates([graph.columns.to_list()])
+
+    
