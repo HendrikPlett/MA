767b0e1b7d7a26fc775fb47bf4d57af70f64952c
HendrikPlett <hendrik.plett@web.de>
2024-06-28 01:50:28 +0200

    Refactored variable split

--- src/causalbenchmark/visualize/nodes.py
+++ src/causalbenchmark/visualize/nodes.py
@@ -1,35 +1,44 @@
 import pandas as pd
 
-from ..util import enforce_valid_adj_mat, variables_increase, enforce_no_duplicates
+from .helper import AdjGraphs
+from ..util import is_sub_adj_mat
+
+_CORE_COL = 'lightblue'
+_DIFF_COL_ADD = 'deepskyblue'
+_DIFF_COL_DEL = 'aliceblue'
+_REST_COL = 'white'
 
 class Nodes:
 
-    def __init__(self, 
-                 ref_graph: pd.DataFrame, 
-                 new_graph: pd.DataFrame, 
-                 all_var: list[str]
+    def __init__(self,
+                 graphs: AdjGraphs
                  ):
         
-        # Validity of input
-        enforce_valid_adj_mat(ref_graph)
-        enforce_valid_adj_mat(new_graph)
-        enforce_no_duplicates(all_var)
-        if not variables_increase(ref_graph.index.to_list(), all_var):
-            raise ValueError("RefGraph has too many variables.")
-        if not variables_increase(new_graph.index.to_list(), all_var):
-            raise ValueError("NewGraph has too many variables.")
+        # Unpack graphs from AdjGraphs object
+        ref_graph = graphs.ref_graph
+        new_graph = graphs.new_graph
+        true_graph = graphs.true_graph
 
-        self._all_var = all_var
         # Check which passed graph is smaller
-        if variables_increase(ref_graph.index.to_list(), new_graph.index.to_list()):
-            self._smaller_graph = ref_graph
-            self._larger_graph = new_graph
-        elif variables_increase(new_graph.index.to_list(), ref_graph.index.to_list()):
-            self._smaller_graph = new_graph
-            self._larger_graph = ref_graph
+        if is_sub_adj_mat(ref_graph, new_graph):
+            smaller_graph_var = ref_graph.index.to_list()
+            larger_graph_var = new_graph.index.to_list()
+            diff_col = _DIFF_COL_ADD 
+        elif is_sub_adj_mat(new_graph, ref_graph):
+            smaller_graph_var = new_graph.index.to_list()
+            larger_graph_var = ref_graph.index.to_list()
+            diff_col = _DIFF_COL_DEL 
         else:
             ValueError("Unclear Error with the passed graphs.")
-
+        
+        # Instantiate self object
+        self._smaller_graph_var = smaller_graph_var
+        self._larger_graph_var = larger_graph_var
+        self._all_var = true_graph.index.to_list()
+        self._core_col = _CORE_COL
+        self._diff_col = diff_col
+        self._rest_col = _REST_COL
+        
         # --- computed later
         self._core_var = []
         self._diff_var = []
@@ -39,7 +48,11 @@ class Nodes:
     def var_split(self):
         return (self._core_var, self._diff_var, self._rest_var)
 
+    @property
+    def var_cols(self):
+        return (self._core_col, self._diff_col, self._rest_col)
+
     def compute_var_groups(self):
-        self._core_var = self._smaller_graph.columns.to_list()
-        self._diff_var = list(set(self._larger_graph).difference(set(self._smaller_graph)))
-        self._rest_var = list(set(self._all_var).difference(set(self._larger_graph)))
+        self._core_var = self._smaller_graph_var
+        self._diff_var = list(set(self._larger_graph_var).difference(self._smaller_graph_var))
+        self._rest_var = list(set(self._all_var).difference(self._larger_graph_var))
