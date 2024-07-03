0c70efd17f49d7fc6d7f7a8862d09ac69200d67e
HendrikPlett <hendrik.plett@web.de>
2024-07-03 15:26:11 +0200

    Added label name

--- src/causalbenchmark/visualize/edgelogic.py
+++ src/causalbenchmark/visualize/edgelogic.py
@@ -21,16 +21,18 @@ def abs_gt(a,b):
 class EdgeLogic:
 
     def __init__(self,
-                 graph_comp, 
-                 true_graph_comp,
+                 graph_comp: function, 
+                 true_graph_comp: function,
                  colormap,
-                 normalizer
+                 normalizer: mcolors.Normalize,
+                 label: str
                  ):
         
         self._graph_comp = graph_comp
         self._true_graph_comp = true_graph_comp
         self._colormap = colormap
         self._normalizer = normalizer
+        self._label = label
 
     @property
     def graph_comp(self):
@@ -47,35 +49,44 @@ class EdgeLogic:
     @property
     def normalizer(self):
         return self._normalizer
+    
+    @property
+    def label(self):
+        return self._label
 
 _all_p = EdgeLogic(graph_comp=gt,
                  true_graph_comp = true,
                  colormap=_COLMAP_ALL,
-                 normalizer=_ZEROONE)    
+                 normalizer=_ZEROONE,
+                 label=None)    
 
 _tp = EdgeLogic(graph_comp=gt,
                true_graph_comp=gt,
                colormap=_COLMAP_TP, 
-               normalizer=_ZEROONE)
+               normalizer=_ZEROONE,
+               label='True Positive in %')
 
 _fp = EdgeLogic(graph_comp=gt,
                true_graph_comp=eq,
                colormap=_COLMAP_FP, 
-               normalizer=_ZEROONE)
+               normalizer=_ZEROONE,
+               label='False Positive in %')
 
 _tp_diff = EdgeLogic(graph_comp=abs_gt,
                      true_graph_comp=gt,
                      colormap=_COLMAP_TP_DIFF,
-                     normalizer=_MINUSONEONE)
+                     normalizer=_MINUSONEONE,
+                     label="True positives: Change in %pts")
 
 _fp_diff = EdgeLogic(graph_comp=abs_gt,
                      true_graph_comp=eq,
                      colormap=_COLMAP_FP_DIFF,
-                     normalizer=_MINUSONEONE)
+                     normalizer=_MINUSONEONE,
+                     label="False positives: Change in %pts")
+
 
 ###
 # Public API
-
 ALL_P = _all_p
 TP = _tp
 FP = _fp
