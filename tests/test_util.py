62d4dd510a8da053f5c6d255b26cd97e6b336fab
HendrikPlett <hendrik.plett@web.de>
2024-08-01 20:27:00 +0200

    Added more tests

--- tests/test_util.py
+++ tests/test_util.py
@@ -11,36 +11,38 @@ class TestDecorators(unittest.TestCase):
 
     def test_measure_time(self):
         @measure_time
-        def sleep_function():
+        def sleep_function(): # Some simple test function
             time.sleep(0.1)
             return 10
         result, runtime = sleep_function()
         self.assertEqual(result, 10)
-        self.assertTrue(runtime > 0.1)
+        self.assertTrue(runtime >= 0.1)
     
 
 class TestListOperations(unittest.TestCase):
 
     def test_has_duplicates(self):
+        # Test on simple made up lists
         self.assertTrue(has_duplicates([1, 2, 3, 2]))
         self.assertFalse(has_duplicates([1, 2, 3, 4]))
         self.assertTrue(has_duplicates(['a', 'b', 'c', 'a']))
         self.assertFalse(has_duplicates([]))
 
     def test_enforce_no_duplicates(self):
-        enforce_no_duplicates([[1, 2, 3], [4, 5, 6]]) # No exception
+        enforce_no_duplicates([[1, 2, 3], [4, 5, 6]]) # Should throw no exception
         with self.assertRaises(ValueError):
-            enforce_no_duplicates([[1, 2, 3], [4, 5, 5]])
+            enforce_no_duplicates([[1, 2, 3], [4, 5, 5]]) # Dublicates -> Should throw ValueError
 
     def test_give_superlist(self):
         self.assertEqual(give_superlist([1, 2, 3, 4], [2, 3]), [1, 2, 3, 4])
         self.assertEqual(give_superlist([2, 3], [1, 2, 3, 4]), [1, 2, 3, 4])
-        self.assertEqual(give_superlist([], [1, 2, 3, 4]), [1, 2, 3, 4])
+        self.assertEqual(give_superlist([], [1, 2, 3, 4]), [1, 2, 3, 4]) # Empty list case
         self.assertEqual(give_superlist([1, 2, 3], [1, 2, 3]), [1, 2, 3])
         with self.assertRaises(ValueError):
-            give_superlist([1, 2, 3], [3, 4, 5])
+            give_superlist([1, 2, 3], [3, 4, 5]) # No superlist exists -> ValueError
 
     def test_give_sublist(self):
+        # Same lists as in test_give_superlist, only that the other list is asserted to be returned
         self.assertEqual(give_sublist([1, 2, 3, 4], [2, 3]), [2, 3])
         self.assertEqual(give_sublist([2, 3], [1, 2, 3, 4]), [2, 3])
         self.assertEqual(give_sublist([], [1, 2, 3, 4]), [])
@@ -68,24 +70,24 @@ class TestDataFrameOperations(unittest.TestCase):
 
     def setUp(self):
         self.df1 = pd.DataFrame({
-            'A': [1, 2, 3],
-            'B': [4, 5, 6]
+            'X': [1, 2, 3, 4],
+            'Y': [101, 102, 103, 104]
         })
         self.df2 = pd.DataFrame({
-            'A': [7, 8, 9],
-            'B': [10, 11, 12]
+            'X': [5, 6, 7],
+            'Y': [105, 106, 107]
         })
         self.df3 = pd.DataFrame({
-            'A': [13, 14, 15],
-            'B': [16, 17, 18]
+            'X': [8, 9],
+            'Y': [108, 109]
         })
         self.df4 = pd.DataFrame({
-            'A': [1.3, 1.9, 14],
-            'B': [0.7, 0.4, 4.1]
+            'X': [1.3, 1.9, 14],
+            'Y': [0.7, 0.4, 4.1]
         })
         self.df_diff_columns = pd.DataFrame({
-            'A': [1, 2, 3],
-            'C': [4, 5, 6]
+            'X': [1, 2, 3],
+            'Z': [101, 102, 103] # Different column name
         })
 
     def test_same_columns(self):
@@ -95,38 +97,40 @@ class TestDataFrameOperations(unittest.TestCase):
             same_columns([])
 
     def test_pool_dfs(self):
-        result1 = pool_dfs([self.df1, self.df2, self.df3])
-        expected_result1 = pd.DataFrame({
-            'A': [1, 2, 3, 7, 8, 9, 13, 14, 15],
-            'B': [4, 5, 6, 10, 11, 12, 16, 17, 18]
+        pooled_dfs = pool_dfs([self.df1, self.df2, self.df3, self.df4])
+        expected_pooled_dfs = pd.DataFrame({
+            'X': [1,2,3,4,5,6,7,8,9,1.3,1.9,14],
+            'Y': [101,102,103,104,105,106,107,108,109,0.7,0.4,4.1]
         })
-        pd.testing.assert_frame_equal(result1, expected_result1)
+        pd.testing.assert_frame_equal(pooled_dfs, expected_pooled_dfs)
 
-        result2 = pool_dfs([self.df1])
-        expected_result2 = pd.DataFrame({
-            'A': [1, 2, 3],
-            'B': [4, 5, 6]
-        })
-        pd.testing.assert_frame_equal(result2, expected_result2)
+        pooled_dfs2 = pool_dfs([self.df1])
+        pd.testing.assert_frame_equal(pooled_dfs2, self.df1)
 
         with self.assertRaises(ValueError):
             pool_dfs([self.df1, self.df_diff_columns])
 
     def test_bootstrap_sample(self):
-        sample_size_fraction = [1/3, 2/3, 1]
+        # Test fraction bootstrap
+        sample_size_fraction = [0.25, 1/3, 1]
         dfs = [self.df1, self.df2, self.df3]
-        result = bootstrap_sample(dfs, sample_size_fraction, seed=1)
-        for df_sample, sample_size, df_orig in zip(result, sample_size_fraction, dfs):
+        bstr_sample1 = bootstrap_sample(dfs, sample_size_fraction, seed=1)
+        self.assertTrue(len(dfs)==len(bstr_sample1))
+        for df_sample, sample_size, df_orig in zip(bstr_sample1, sample_size_fraction, dfs):
             self.assertEqual(len(df_sample), int(len(df_orig) * sample_size))
+            self.assertTrue(same_columns([df_sample, df_orig]))
 
-        sample_size_integer = [2, 2, 2]
-        result = bootstrap_sample([self.df1, self.df2, self.df3], sample_size_integer, seed=1)
-        for df, sample_size in zip(result, sample_size_integer):
-            self.assertEqual(len(df), sample_size)
+        # Test absolute number bootstrap
+        sample_size_integer = [3, 2, 2] 
+        bstr_sample2 = bootstrap_sample(dfs, sample_size_integer, seed=1)
+        self.assertTrue(len(dfs)==len(bstr_sample2))
+        for df_sample, sample_size, df_original in zip(bstr_sample2, sample_size_integer, dfs):
+            self.assertEqual(len(df_sample), sample_size)
+            self.assertTrue(same_columns([df_sample, df_original]))
 
-        invalid_sample_size = [-1, 0.5, 2]
+        false_sample_size = [-1, 0.5, 2]
         with self.assertRaises(ValueError):
-            bootstrap_sample([self.df1, self.df2, self.df3], invalid_sample_size, seed=1)
+            bootstrap_sample([self.df1, self.df2, self.df3], false_sample_size, seed=1)
 
         with self.assertRaises(AssertionError):
             bootstrap_sample([self.df1, self.df2], [0.5, 0.5, 0.5], seed=1)
@@ -164,8 +168,8 @@ class TestDataFrameOperations(unittest.TestCase):
         # Case of a single dataframe
         df_stand_single = standardize_dfs([self.df4])
         self.assertEqual(len(df_stand_single), 1)
-        self.assertAlmostEqual(df_stand_single[0]['A'].mean(), 0, places=5)
-        self.assertAlmostEqual(df_stand_single[0]['A'].std(), 1, places=5)
+        self.assertAlmostEqual(df_stand_single[0]['X'].mean(), 0, places=5)
+        self.assertAlmostEqual(df_stand_single[0]['X'].std(), 1, places=5)
         pd.testing.assert_frame_equal(self.df4.corr(), df_stand_single[0].corr())
         
         # Ensure that original data is unchanged and new data is created
@@ -177,12 +181,12 @@ class TestDataFrameOperations(unittest.TestCase):
 class TestAdjacencyMatrixDataFrameOperations(unittest.TestCase):
     
     def setUp(self):
-        self.df1 = pd.DataFrame(
+        self.valid_df1 = pd.DataFrame(
             [[1, 0, 0], [0, 1, 1], [0, 1, 1]],
             index=['a', 'b', 'c'],
             columns=['a', 'b', 'c']
         )
-        self.df2 = pd.DataFrame(
+        self.valid_df2 = pd.DataFrame(
             [[1, 0, 0, 0], [0, 1, 1, 0], [0, 1, 1, 0], [0, 0, 0, 1]],
             index=['a', 'b', 'c', 'd'],
             columns=['a', 'b', 'c', 'd']
@@ -194,7 +198,7 @@ class TestAdjacencyMatrixDataFrameOperations(unittest.TestCase):
         )
         self.df_invalid2 = pd.DataFrame(
             [[1, 0], [0, 1]],
-            index=['a', 'a'], # Same names
+            index=['a', 'a'], # Same index names
             columns=['a', 'b']  
         )
         self.binary_df = pd.DataFrame(
@@ -215,24 +219,24 @@ class TestAdjacencyMatrixDataFrameOperations(unittest.TestCase):
         
 
     def test_is_sub_adj_mat(self):
-        self.assertTrue(is_sub_adj_mat(self.df1, self.df2))
-        self.assertFalse(is_sub_adj_mat(self.df2, self.df1))
+        self.assertTrue(is_sub_adj_mat(self.valid_df1, self.valid_df2))
+        self.assertFalse(is_sub_adj_mat(self.valid_df2, self.valid_df1))
         with self.assertRaises(ValueError):
-            is_sub_adj_mat(self.df_invalid1, self.df2)
+            is_sub_adj_mat(self.df_invalid1, self.valid_df2)
         with self.assertRaises(ValueError):
-            is_sub_adj_mat(self.df_invalid2, self.df2)
+            is_sub_adj_mat(self.df_invalid2, self.valid_df2)
 
     def test_enforce_sub_adj_mat(self):
-        enforce_sub_adj_mat(self.df1, self.df2)
+        enforce_sub_adj_mat(self.valid_df1, self.valid_df2)
         with self.assertRaises(ValueError):
-            enforce_sub_adj_mat(self.df2, self.df1)
+            enforce_sub_adj_mat(self.valid_df2, self.valid_df1)
         with self.assertRaises(ValueError):
-            enforce_sub_adj_mat(self.df_invalid1, self.df2)
+            enforce_sub_adj_mat(self.df_invalid1, self.valid_df2)
         with self.assertRaises(ValueError):
-            enforce_sub_adj_mat(self.df_invalid2, self.df2)
+            enforce_sub_adj_mat(self.df_invalid2, self.valid_df2)
 
     def test_reduce_to_size(self):
-        reduced = reduce_to_size(self.df2, self.df1)
+        reduced = reduce_to_size(self.valid_df2, self.valid_df1)
         expected_reduced = pd.DataFrame(
             [[1, 0, 0], [0, 1, 1], [0, 1, 1]],
             index=['a', 'b', 'c'],
@@ -240,10 +244,10 @@ class TestAdjacencyMatrixDataFrameOperations(unittest.TestCase):
         )
         pd.testing.assert_frame_equal(reduced, expected_reduced)
         with self.assertRaises(ValueError):
-            reduce_to_size(self.df1, self.df2)
+            reduce_to_size(self.valid_df1, self.valid_df2)
 
     def test_pad_zeros_to_size(self):
-        padded = pad_zeros_to_size(self.df1, self.df2)
+        padded = pad_zeros_to_size(self.valid_df1, self.valid_df2)
         expected_padded = pd.DataFrame(
             [[1, 0, 0, 0], [0, 1, 1, 0], [0, 1, 1, 0], [0, 0, 0, 0]],
             index=['a', 'b', 'c', 'd'],
@@ -251,10 +255,10 @@ class TestAdjacencyMatrixDataFrameOperations(unittest.TestCase):
         )
         pd.testing.assert_frame_equal(padded, expected_padded)
         with self.assertRaises(ValueError):
-            pad_zeros_to_size(self.df2, self.df1)
+            pad_zeros_to_size(self.valid_df2, self.valid_df1)
 
     def test_enforce_valid_bstr_adj_mat(self):
-        enforce_valid_bstr_adj_mat(self.df1)
+        enforce_valid_bstr_adj_mat(self.valid_df1)
         with self.assertRaises(ValueError):
             enforce_valid_bstr_adj_mat(self.outsidezeroone_df1)
         with self.assertRaises(ValueError):
@@ -270,8 +274,8 @@ class TestAdjacencyMatrixDataFrameOperations(unittest.TestCase):
             enforce_binary_adj_mat(self.outsidezeroone_df2)
 
     def test_enforce_valid_adj_mat(self):
-        enforce_valid_adj_mat(self.df1)
-        enforce_valid_adj_mat(self.df2)
+        enforce_valid_adj_mat(self.valid_df1)
+        enforce_valid_adj_mat(self.valid_df2)
         with self.assertRaises(ValueError):
             enforce_valid_adj_mat(self.df_invalid1)
         with self.assertRaises(ValueError):
