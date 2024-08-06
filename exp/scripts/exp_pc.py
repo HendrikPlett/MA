039af19e28c7fc059a7fd164a9c35d972472dd46
HendrikPlett <hendrik.plett@web.de>
2024-08-06 18:30:04 +0200

    Utilized predefined test functions

--- exp/scripts/exp_pc.py
+++ exp/scripts/exp_pc.py
@@ -1,4 +1,4 @@
-from config import (
+from .assistant import (
     # Precreated datasets/true DAGs
     SMALL_VAR_TRUE_DAG,
     MID_VAR_TRUE_DAG,
@@ -9,8 +9,15 @@ from config import (
     RED_UNIFORM_REFERENCE,
     RED_GREEN_UNIFORM_REFERENCE,
     # Other useful constants
-    OBS_DATA_SIZES,
+    NR_BOOTSTRAPS,
+    OBS_DATA_COMP_SIZES,
     DEFAULT_DATA_SIZE,
+    # Predefined benchmarking functions
+    increase_obs_data_small_var,
+    increase_obs_data_mid_var,
+    increase_variables,
+    increase_colors,
+    increase_hyperparameter,
     # Benchmarking classes
     BootstrapComparison,
     Bootstrap,
@@ -25,93 +32,34 @@ ALPHA = 0.1
 ### ------------------------------------------------------
 # Actual jobs
 
-def increase_obs_data_small_var(sizes: list[int]):
-    bstrpcomp = BootstrapComparison("PC-SmallVar-IncreaseObservationalData")
-    for size in sizes:
-        bstrpcomp.add_bootstrap(
-            Bootstrap(
-                name=f"n = {size}",
-                true_dag=SMALL_VAR_TRUE_DAG,
-                algorithm=PC(alpha=ALPHA),
-                data_to_bootstrap_from=SMALL_VAR_UNIFORM_REFERENCE,
-                sample_sizes=[size],
-            )
-        )
-    bstrpcomp.run_comparison()
-    bstrpcomp.pickle()
-
-
-def increase_obs_data_mid_var(sizes: list[int]):
-    bstrpcomp = BootstrapComparison("PC-MidVar-IncreaseObservationalData")
-    for size in sizes:
-        bstrpcomp.add_bootstrap(
-            Bootstrap(
-                name=f"n = {size}",
+def independence_test_comparison():
+    bstrcomp = BootstrapComparison("PC_CiTestComparison")
+    bstrcomp.add_bootstrap(
+        Bootstrap(
+                name=f"CI Test: FisherZ",
                 true_dag=MID_VAR_TRUE_DAG,
-                algorithm=PC(alpha=ALPHA),
-                data_to_bootstrap_from=MID_VAR_UNIFORM_REFERENCE,
-                sample_sizes=[size],
-            )
-        )
-    bstrpcomp.run_comparison()
-    bstrpcomp.pickle()
-
-def increase_variables():
-    names = ['Small Var', 'Med Var']
-    true_dags = [SMALL_VAR_TRUE_DAG, MID_VAR_TRUE_DAG]
-    datas = [SMALL_VAR_UNIFORM_REFERENCE, MID_VAR_UNIFORM_REFERENCE]
-    bstrpcomp = BootstrapComparison("PC-IncreaseVariableCount")
-    for name, true_dag, data in zip(names, true_dags, datas):
-        bstrpcomp.add_bootstrap(
-            Bootstrap(
-                name=name,
-                true_dag=true_dag,
-                algorithm=PC(alpha=ALPHA),
-                data_to_bootstrap_from=data,
-                sample_sizes=DEFAULT_DATA_SIZE,
-            )
-        )
-    bstrpcomp.run_comparison()
-    bstrpcomp.pickle()
-
-def increase_colors():
-    names = ['Red', 'Red, Green', 'Red, Green, Blue']
-    true_dags = [RED_TRUE_DAG, RED_GREEN_TRUE_DAG, MID_VAR_TRUE_DAG]
-    datas = [RED_UNIFORM_REFERENCE, RED_GREEN_UNIFORM_REFERENCE, MID_VAR_UNIFORM_REFERENCE]
-    bstrpcomp = BootstrapComparison("PC-IncreaseColorCount")
-    for name, true_dag, data in zip(names, true_dags, datas):
-        bstrpcomp.add_bootstrap(
-            Bootstrap(
-                name=name,
-                true_dag=true_dag,
-                algorithm=PC(alpha=ALPHA),
-                data_to_bootstrap_from=data,
-                sample_sizes=DEFAULT_DATA_SIZE,
-            )
-        )
-    bstrpcomp.run_comparison()
-    bstrpcomp.pickle()
-
-def increase_alpha(alphas: list):
-    bstrpcomp = BootstrapComparison("PC-IncreaseAlpha")
-    for alpha in alphas:
-        bstrpcomp.add_bootstrap(
-            Bootstrap(
-                name=f"Alpha: {alpha}",
-                true_dag=MID_VAR_TRUE_DAG,
-                algorithm=PC(alpha=alpha),
+                algorithm=PC(alpha=ALPHA, indep_test="fisherz"),
                 data_to_bootstrap_from=MID_VAR_UNIFORM_REFERENCE,
                 sample_sizes=DEFAULT_DATA_SIZE,
-            )
+                nr_bootstraps=NR_BOOTSTRAPS        
         )
-    bstrpcomp.run_comparison()
-    bstrpcomp.pickle()
-
+    )
+    bstrcomp.add_bootstrap(
+        Bootstrap(
+                name=f"CI Test: FisherZ",
+                true_dag=MID_VAR_TRUE_DAG,
+                algorithm=PC(alpha=ALPHA, indep_test="kci"),
+                data_to_bootstrap_from=MID_VAR_UNIFORM_REFERENCE,
+                sample_sizes=DEFAULT_DATA_SIZE,
+                nr_bootstraps=NR_BOOTSTRAPS        
+        )
+    )
 
 if __name__ == "__main__":
-    increase_obs_data_small_var(sizes=OBS_DATA_SIZES)
-    increase_obs_data_mid_var(sizes=OBS_DATA_SIZES)
-    increase_variables()
-    increase_colors()
-    increase_alpha(alphas=[0.001, 0.01, 0.05, 0.1, 0.2, 0.4, 0.6])
+    increase_obs_data_small_var(alg=PC(alpha=ALPHA))
+    increase_obs_data_mid_var(alg=PC(alpha=ALPHA))
+    increase_variables(alg=PC(alpha=ALPHA))
+    increase_colors(alg=PC(alpha=ALPHA))
+    increase_hyperparameter(cls=PC, change_param_dict={'alpha': [0.001, 0.01, 0.05, 0.1, 0.2, 0.4, 0.6]})
+    independence_test_comparison()
 
