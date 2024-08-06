7a867d52ac4cf5b2773215240645f751192e7b2b
HendrikPlett <hendrik.plett@web.de>
2024-08-06 15:41:26 +0200

    Created more datasets

--- exp/scripts/config.py
+++ exp/scripts/config.py
@@ -9,37 +9,79 @@ sys.path.append(src_path)
 from causalbenchmark.compute.bootstrap import BootstrapComparison, Bootstrap
 from causalbenchmark.compute.algorithms import PC
 from cc_wrapper import CCWrapper
-from cc_wrapper import SMALL_VAR, MID_VAR, ALL_VAR, LATEX_NAME
+from cc_wrapper import SMALL_VAR, MID_VAR, ALL_VAR
+
+### ------------------------------------------------------
+# Get some frequently used CausalChamber data
+### ------------------------------------------------------
 
 
 ### ------------------------------------------------------
-# Get CausalChamber data
+# lt_interventions, uniform reference, predefined variable sets
+_EXP_FAMILY = "lt_interventions_standard_v1"
+_EXPERIMENT = ["uniform_reference"]
+_SIZE = [1000]
 
-ccw = CCWrapper()
-ccw.set_exp_family("lt_interventions_standard_v1")
-ccw.set_variables(SMALL_VAR)
+_ccw = CCWrapper()
+_ccw.set_exp_family(_EXP_FAMILY)
+_ccw.set_variables(SMALL_VAR)
 # Fetch True DAG
-SMALL_VAR_TRUE_DAG = ccw.fetch_true_dag()
+_SMALL_VAR_TRUE_DAG = _ccw.fetch_true_dag()
 # Fetch data
-SMALL_VAR_UNIFORM_REFERENCE = ccw.fetch_experiments(
-                    experiments=['uniform_reference'], 
-                    sizes=[10000]
+_SMALL_VAR_UNIFORM_REFERENCE = _ccw.fetch_experiments(
+                    experiments=_EXPERIMENT, 
+                    sizes=_SIZE
                     ) # Returns list
 
-ccw.set_variables(MID_VAR)
+_ccw.set_variables(MID_VAR)
 # Fetch True DAG
-MID_VAR_TRUE_DAG = ccw.fetch_true_dag()
+_MID_VAR_TRUE_DAG = _ccw.fetch_true_dag()
 # Fetch data
-MID_VAR_UNIFORM_REFERENCE = ccw.fetch_experiments(
-                    experiments=['uniform_reference'], 
-                    sizes=[10000]
+_MID_VAR_UNIFORM_REFERENCE = _ccw.fetch_experiments(
+                    experiments=_EXPERIMENT, 
+                    sizes=_SIZE
                     ) # Returns list
 
-ccw.set_variables(ALL_VAR)
+_ccw.set_variables(ALL_VAR)
 # Fetch True DAG
-ALL_VAR_TRUE_DAG = ccw.fetch_true_dag()
+_ALL_VAR_TRUE_DAG = _ccw.fetch_true_dag()
 # Fetch data
-ALL_VAR_UNIFORM_REFERENCE = ccw.fetch_experiments(
-                    experiments=['uniform_reference'], 
-                    sizes=[10000]
+_ALL_VAR_UNIFORM_REFERENCE = _ccw.fetch_experiments(
+                    experiments=_EXPERIMENT, 
+                    sizes=_SIZE
                     ) # Returns list
+
+
+# --- Public API ---
+SMALL_VAR_TRUE_DAG = _SMALL_VAR_TRUE_DAG
+SMALL_VAR_UNIFORM_REFERENCE = _SMALL_VAR_UNIFORM_REFERENCE
+MID_VAR_TRUE_DAG = _MID_VAR_TRUE_DAG
+MID_VAR_UNIFORM_REFERENCE = _MID_VAR_UNIFORM_REFERENCE
+ALL_VAR_TRUE_DAG = _ALL_VAR_TRUE_DAG
+ALL_VAR_UNIFORM_REFERENCE = _ALL_VAR_UNIFORM_REFERENCE
+# --- Public API ---
+
+
+### ------------------------------------------------------
+# lt_interventions, uniform reference, drop colors
+
+_ccw.set_variables([var for var in MID_VAR if var not in ['green', 'blue']])
+_RED_TRUE_DAG = _ccw.fetch_true_dag()
+_RED_UNIFORM_REFERNECE = _ccw.fetch_experiments(
+                experiments=_EXPERIMENT,
+                sizes=_SIZE
+            )
+
+_ccw.set_variables([var for var in MID_VAR if var not in ['blue']])
+_RED_GREEN_TRUE_DAG = _ccw.fetch_true_dag()
+_RED_GREEN_UNIFORM_REFERNECE = _ccw.fetch_experiments(
+                experiments=_EXPERIMENT,
+                sizes=_SIZE
+            )
+
+# --- Public API ---
+RED_TRUE_DAG = _RED_TRUE_DAG
+RED_UNIFORM_REFERENCE = _RED_UNIFORM_REFERNECE
+RED_GREEN_TRUE_DAG = _RED_GREEN_TRUE_DAG
+RED_GREEN_UNIFORM_REFERENCE = _RED_GREEN_UNIFORM_REFERNECE
+# --- Public API ---
