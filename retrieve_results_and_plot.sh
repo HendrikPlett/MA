1a7b1d005f4bddc440147c520d45525009ee6141
HendrikPlett <hendrik.plett@web.de>
2024-08-08 17:05:43 +0200

    Added option to retrieve subset of .pkl files

--- retrieve_results_and_plot.sh
+++ retrieve_results_and_plot.sh
@@ -1,10 +1,17 @@
 # Run in the current shell using "source retrieve_results_and_make_plots.sh"
 
 LOCAL_HOME_PATH="/Users/hendrikplett/Desktop/3. Uni/3.Master_Statistik/4. Semester/Masterarbeit/3. Code"
+STRING_FILTER=$1 # Retrieve only pickle files that start with this passed String
+
 
 # Retrieve results from EULER
 cd "$LOCAL_HOME_PATH"
-rsync -avz --include='*.pkl' --exclude='*' hplett@euler.ethz.ch:/cluster/home/hplett/MA/exp/results/ ./MA/exp/results/
+
+if [ -z "$STRING_FILTER" ]; then
+    rsync -avz --include='*.pkl' --exclude='*' hplett@euler.ethz.ch:/cluster/home/hplett/MA/exp/results/ ./MA/exp/results/
+else
+    rsync -avz --include="$STRING_FILTER*.pkl" --exclude='*' hplett@euler.ethz.ch:/cluster/home/hplett/MA/exp/results/ ./MA/exp/results/
+fi
 
 # Create plots
 cd "$LOCAL_HOME_PATH"
