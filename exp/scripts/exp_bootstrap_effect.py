f90ae3acc71bb0f41da3ebef730c1470da17fea6
HendrikPlett <hendrik.plett@web.de>
2024-08-10 20:44:41 +0200

    Initial commit

---
+++ exp/scripts/exp_bootstrap_effect.py
@@ -0,0 +1,45 @@
+from exp_assistant import (
+    # Precreated datasets/true DAGs
+    MID_VAR_TRUE_DAG,
+    MID_VAR_UNIFORM_REFERENCE_1000_SAMPLE,
+    MID_VAR_UNIFORM_REFERENCE_2000_SAMPLE,
+    MID_VAR_UNIFORM_REFERENCE_3000_SAMPLE,
+    MID_VAR_UNIFORM_REFERENCE,
+    # Other useful constants
+    NR_BOOTSTRAPS,
+    DEFAULT_DATA_SIZE,
+    # Benchmarking classes
+    BootstrapComparison,
+    Bootstrap,
+    # Algorithm 
+    GES
+)
+
+PROCESSES = 50
+
+
+def test_empirical_distribution_size(processes: int = PROCESSES):
+    bstrpcomp = BootstrapComparison("Influence-EmpiricalDistributionSize-on-GES")
+    datasets = [MID_VAR_UNIFORM_REFERENCE_1000_SAMPLE, 
+                 MID_VAR_UNIFORM_REFERENCE_2000_SAMPLE,
+                 MID_VAR_UNIFORM_REFERENCE_3000_SAMPLE,
+                 MID_VAR_UNIFORM_REFERENCE]
+    names = [1000, 2000, 3000, 10000]
+    for data, name in zip(datasets, names):
+        bstrpcomp.add_bootstrap(
+            Bootstrap(
+                name=name,
+                true_dag=MID_VAR_TRUE_DAG,
+                algorithm=GES(),
+                data_to_bootstrap_from=data,
+                sample_sizes=DEFAULT_DATA_SIZE*len(data),
+                nr_bootstraps=NR_BOOTSTRAPS,
+                PROCESSES = processes
+            )
+        )
+    bstrpcomp.run_comparison()
+    bstrpcomp.pickle()
+
+
+if __name__ == "__main__":
+    test_empirical_distribution_size()
