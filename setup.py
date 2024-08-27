a6eb8203d543ef52768fa34d927d273560413446
HendrikPlett <hendrik.plett@web.de>
2024-08-27 23:57:00 +0200

    Bugfix

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
-setuptools.setup(
+setup(
     name='causalbenchmark',
     version='0.1.0',
     author='Hendrik Plett',
     author_email='hendrik.plett@web.de',
-    packages=['src.causalbenchmark', 'src.causalbenchmark.compute', 'src.causalbenchmark.visualize'],
-    scripts=[],
+    description='Benchmarking causal discovery algorithms on the edge level.',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
     url='https://github.com/HendrikPlett/MA',
-    description='Python implementation of the GES algorithm for causal discovery',
-    install_requires=requirements
+    install_requires=requirements,
+    python_requires='>=3.6'
 )
