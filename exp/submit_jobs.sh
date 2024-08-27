de2a2c93cf7cf352295865c81f33dc05b4d15999
HendrikPlett <hendrik.plett@web.de>
2024-08-27 16:12:03 +0200

    Added all jobs

--- exp/submit_jobs.sh
+++ exp/submit_jobs.sh
@@ -8,8 +8,9 @@ submit_job() {
     
     sbatch \
         --job-name="$(basename "$script" .py)" \
-        --ntasks=1 \
-        --cpus-per-task="$cpus" \
+        --ntasks="$cpus" \
+        --ntasks-per-node="$cpus" \
+        --cpus-per-task=1 \
         --mem-per-cpu="$mem_per_cpu" \
         --time="$time" \
         --mail-type=BEGIN,END \
@@ -23,5 +24,19 @@ submit_job() {
 module load stack/2024-04 gcc/8.5.0 python/3.9.18
 source $HOME/maplett/bin/activate
 
+export OMP_NUM_THREADS=1
+export CUDA_VISIBLE_DEVICES=""
+
 # Submit the wanted scripts
-submit_job "scripts/exp_pc.py" 8 2048 "04:00:00"
+submit_job "scripts/exp_pc.py" 52 2048 "00:20:00"
+submit_job "scripts/exp_pc_kci.py" 102 4096 "02:00:00"
+submit_job "scripts/exp_ges.py" 52 2048 "00:20:00"
+submit_job "scripts/exp_notears.py" 52 4096 "00:40:00"
+submit_job "scripts/exp_golem.py" 22 8192 "04:00:00"
+submit_job "scripts/exp_icp_small_var.py" 52 2048 "00:20:00"
+submit_job "scripts/exp_score_based_comparison.py" 22 8192 "06:00:00"
+submit_job "scripts/exp_GniES_comparison.py" 102 2048 "02:00:00"
+submit_job "scripts/exp_bootstrap_effect.py" 52 2028 "00:20:00"
+submit_job "scripts/exp_gies.py" 52 2048 "01:00:00"
+submit_job "scripts/exp_gnies.py" 102 2048 "12:00:00"
+submit_job "scripts/exp_utigsp.py" 102 2048 "12:00:00"
